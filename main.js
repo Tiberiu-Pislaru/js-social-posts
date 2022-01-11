446bdb93981df76e24fe550661ac079c849cbcb6
Tiberiu-Pislaru <tiberiupislaru03@gmail.com>
2022-01-11 14:54:16 +0100

    Fatta la prima Milestone

---
+++ main.js
@@ -0,0 +1,22 @@
+
+// Milestone 1
+// creiamo il array di oggetti
+const posts=[
+    {
+        idPost:1,
+        
+        pictureProfile:'https://media.istockphoto.com/photos/elephants-fear-of-mice-picture-id155439372?b=1&k=20&m=155439372&s=170667a&w=0&h=4BMK9d2lxIvBFNRDx-T_JproEaxG2O1P38XudI2Wfkg=',
+        
+        date:'01-11-2022',
+        
+        text:'Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.',
+        
+        image: 'https://images.unsplash.com/photo-1641157141085-8454fbc33f3c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDd8Ym84alFLVGFFMFl8fGVufDB8fHx8&auto=format&fit=crop&w=600&q=60',
+        
+        likes: 0,
+    }
+];
+
+
+//Milestone 2
+// stampiamo i post nel feed
