7f892acd55bbb2ddc3705178b0a7acbf4235940e
Tiberiu-Pislaru <tiberiupislaru03@gmail.com>
2022-01-11 16:00:04 +0100

    Finita la Milestone 2

--- main.js
+++ main.js
@@ -3,7 +3,7 @@
 // creiamo il array di oggetti
 const posts=[
     {
-        idPost:1,
+        idPost:10,
         
         pictureProfile:'https://media.istockphoto.com/photos/elephants-fear-of-mice-picture-id155439372?b=1&k=20&m=155439372&s=170667a&w=0&h=4BMK9d2lxIvBFNRDx-T_JproEaxG2O1P38XudI2Wfkg=',
         
@@ -20,3 +20,50 @@ const posts=[
 
 //Milestone 2
 // stampiamo i post nel feed
+// prima prendiamo dal HTML il container dove saranno stampati i post
+
+const containerPosts = document.getElementById('container');
+
+for (let i = 0; i < posts.length; i++) {
+    const post = posts[i];
+
+    containerPosts.innerHTML+= `
+    <div class="post">
+        <div class="post__header">
+            <div class="post-meta">                    
+                <div class="post-meta__icon">
+                    <img class="profile-pic" src="${post.pictureProfile}" alt="Phil Mangione">                    
+                </div>
+                <div class="post-meta__data">
+                    <div class="post-meta__author">Phil Mangione</div>
+                    <div class="post-meta__time">${post.date}</div>
+                </div>                    
+            </div>
+        </div>
+        <div class="post__text">${post.text}</div>
+        <div class="post__image">
+            <img src="${post.image}" alt="">
+        </div>
+        <div class="post__footer">
+            <div class="likes js-likes">
+                <div class="likes__cta">
+                    <a class="like-button  js-like-button" href="#" data-postid="${post.idPost}">
+                        <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                        <span class="like-button__label">Mi Piace</span>
+                    </a>
+                </div>
+                <div class="likes__counter">
+                    Piace a <b id="like-counter-1" class="js-likes-counter">${post.likes}</b> persone
+                </div>
+            </div> 
+        </div>            
+    </div>`
+}
+
+// Milestone 3
+// il colore del tasto 'Mi Piace' cambia colore appena viene cliccato
+
+const likeButton = document.querySelector('.js-like-button');
+likeButton.addEventListener('click', function(){
+    
+})
